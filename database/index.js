d9133ffbe56f6e64cce16152e1797d37d23e2131
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-06-13 14:14:57 -0700

    change to procfile and monog url

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher',{
+// mongodb+srv://Eduardo:Cruzazul$10@cluster0-ck1u1.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/fetcher',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 } );
